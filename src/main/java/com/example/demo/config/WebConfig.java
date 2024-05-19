be1e3c10cec55190caa2bbf01b32916a5701e9bb
Abderrahmen HELAOUI <41267717+Abderahmen8HELAOUI@users.noreply.github.com>
2024-05-19 21:06:54 +0100

    change front end url

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://applicationlivredecaisseclient-c45a7f748dd6.herokuapp.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
